15cbf556364137a19f12a5a2f952fe8f37e57504
Antony <antmaster26305@gmail.com>
2021-10-06 10:19:19 +1100

    Search Bar Update

--- searchBars/searchActor.php
+++ searchBars/searchActor.php
@@ -3,7 +3,7 @@
         <h3 class="text-center">Search For Any Actor</h3>
         <form class="searchForm " id="searchForm" method="POST">
             <!--Search Box-->
-            <input class="form-control" type="text" id="searchActor" placeholder="Search Movies..." >
+            <input class="form-control" type="text" id="searchActor" placeholder="Search Actors..." >
         </form>
     </div>
 </div>
--- searchBars/searchTVTitle.php
+++ searchBars/searchTVTitle.php
@@ -3,7 +3,7 @@
         <h3 class="text-center">Search For Any TV Show Title</h3>
         <form class="searchForm " id="searchForm" method="POST">
             <!--Search Box-->
-            <input class="form-control" type="text" id="searchTV" placeholder="Search Movies..." >
+            <input class="form-control" type="text" id="searchTV" placeholder="Search TV Shows..." >
         </form>
     </div>
 </div>
